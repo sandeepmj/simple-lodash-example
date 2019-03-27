2aa9a310e6acd60d9e4a9c25d183ddd1a9529aa0
Sandeep <sjnews@gmail.com>
2019-03-26 22:10:53 -0400

    added row

--- data.js
+++ data.js
@@ -6,7 +6,8 @@ let data = [{
 		"fav_sport": "Football",
 		"fav_movie": "Exorcist",
 		"fav_song": "Rain",
-		"fav_animal": "Wolves"
+		"fav_animal": "Wolves",
+		"fav_drink": "Rum"
 	},
 	{
 		"name": "Jane",
@@ -14,7 +15,8 @@ let data = [{
 		"fav_sport": "Baseball",
 		"fav_movie": "Batman",
 		"fav_song": "Rain Dogs",
-		"fav_animal": "Cats"
+		"fav_animal": "Cats",
+		"fav_drink": "Wine"
 	},
 	{
 		"name": "Sandeep",
@@ -22,15 +24,17 @@ let data = [{
 		"fav_sport": "Tennis",
 		"fav_movie": "The Lives of Others",
 		"fav_song": "A Day in the Life",
-		"fav_animal": "Dogs"
-  },
+		"fav_animal": "Dogs",
+		"fav_drink": "Scotch"
+	},
 	{
 		"name": "Christine",
 		"fav_food": "Japanese",
 		"fav_sport": "Tennis",
 		"fav_movie": "Wonder Woman",
 		"fav_song": "The Boxer",
-		"fav_animal": "Dogs"
+		"fav_animal": "Dogs",
+		"fav_drink": "Wine"
 	},
 	{
 		"name": "Krithi",
@@ -38,7 +42,8 @@ let data = [{
 		"fav_sport": "Soccer",
 		"fav_movie": "Ghost Busters",
 		"fav_song": "Hells Bells",
-		"fav_animal": "Cats"
+		"fav_animal": "Cats",
+		"fav_drink": "Seltzer"
 	},
 	{
 		"name": "Sulekha",
@@ -46,7 +51,8 @@ let data = [{
 		"fav_sport": "Tennis",
 		"fav_movie": "Mary Poppins",
 		"fav_song": "This Girl Is on Fire",
-		"fav_animal": "Dolphins"
+		"fav_animal": "Dolphins",
+		"fav_drink": "Orange juice"
 	},
 	{
 		"name": "Bill",
@@ -54,6 +60,16 @@ let data = [{
 		"fav_sport": "Football",
 		"fav_movie": "Mary Poppins",
 		"fav_song": "Help",
-		"fav_animal": "Horse"
+		"fav_animal": "Horse",
+		"fav_drink": "vodka"
+	},
+	{
+		"name": "TC",
+		"fav_food": "Burgers",
+		"fav_sport": "Football",
+		"fav_movie": "Lion King",
+		"fav_song": "Brick",
+		"fav_animal": "Parrots",
+		"fav_drink": "beer"
 	}
 ];
